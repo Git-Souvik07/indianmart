d8b39397d9686e1b5e550743bfc8ed9cffa0c0af
Git-Souvik07 <souvikdashpl123@gmail.com>
2025-05-16 16:17:04 +0530

    Updated products.ts

--- src/data/products.ts
+++ src/data/products.ts
@@ -698,7 +698,7 @@ export const products: Product[] = [
     name: 'Ice Cream',
     description: 'Delicious and creamy ice cream, available in various flavors.',
     price: 100, // Price in rupees per 500ml
-    image: 'https://images.pexels.com/photos/1278686/pexels-photo-1278686.jpeg?auto=compress&cs=tinysrgb&w=600',
+    image: 'https://www.keep-calm-and-eat-ice-cream.com/wp-content/uploads/2022/08/Ice-cream-sundae-hero-11.jpg'',
     category: 'Dairy',
     stock: 40,
     rating: 4.7,
