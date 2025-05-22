45fcb295689150e2b9ad2c3fb31ca9638fe55dea
Git-Souvik07 <souvikdashpl123@gmail.com>
2025-05-22 13:30:24 +0530

    Deploy application to Netlify

--- src/data/products.ts
+++ src/data/products.ts
@@ -42,17 +42,7 @@ export const products: Product[] = [
     rating: 4.8,
     isPopular: true
   },
- {
-    id: 6,
-    name: 'Turmeric Powder',
-    description: 'Natural turmeric powder for cooking and medicinal purposes.',
-    price: 50, // Price in rupees per 100g
-    image: ',
-    category: 'Spices',
-    stock: 120,
-    rating: 4.5,
-    isPopular: false
-  },
+ 
   {
     id: 69,
     name: 'Apples',
